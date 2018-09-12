43cdc9b92e11b30b7f6bad4f4a66cb8bc807eae1
ciaomarian <ciaomarian@gmail.com>
2018-09-12 13:28:17 -0700

    Added BookShelves.js additional component. (doesn't work)

---
+++ src/BookShelves.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import './App.css';
+import Book from './Book';
+
+
+
+class BookShelves extends Component {
+    render() {
+            return (
+            
+
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                     {
+                      this.props.books
+                      .filter(book => book.shelf === 'currentlyReading')
+                      .map(book =>
+                        <li key = {book.id}>< Book book = {book} 
+                          moveShelf = {this.props.moveShelf}
+                        currentShelf = 'currentlyReading'
+                        />
+                      </li>)
+                      }
+                    </ol>
+                  </div>
+                </div>
+
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                    {
+                      this.props.books
+                      .filter(book => book.shelf === 'wantToRead')
+                      .map(book =>
+                        <li key = {book.id}>
+                        < Book book={book}
+                        moveShelf = {
+                          this.props.moveShelf
+                        }
+                        currentShelf = 'wantToRead'
+                        />
+                      </li>)
+                    }                     
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {
+                      this.props.books
+                      .filter(book => book.shelf === 'read')
+                      .map(book =>
+                        <li key = {book.id}>
+                        <Book book={book}
+                        moveShelf = {
+                          this.props.moveShelf}
+                        currentShelf = 'read'
+                        />
+                      </li>)
+                      }
+                    </ol>
+                  </div>
+                </div>
+              </div> 
+        );
+    }
+}
+export default BookShelves;
